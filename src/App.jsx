58b4e5eb2a881e55973dd506f244abb2b83ca85d
artexe1 <artexe1@gmail.com>
2026-06-13 16:45:25 +0200

    initial: placeholder page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="placeholder">
+      <h1>Coming soon</h1>
+      <p>Portfolio is under construction.</p>
+      <a href="https://github.com/artexe1" target="_blank" rel="noreferrer">
+        github.com/artexe1
+      </a>
+    </div>
+  )
+}
